451a525e79404e256dc338cd2aa396fe191a590c
krismin18 <46347727+krismin18@users.noreply.github.com>
2019-02-26 19:40:01 +0530

    Add files via upload

---
+++ SampleProject.java
@@ -0,0 +1,20 @@
+package main;
+
+public class SampleProject {
+	public static String printHello() {
+		//print Hello world! in the console!
+		String c = "";
+		c = "Hello world!";
+				
+		return c;
+	}
+	public static int add(int a,int b) {
+		int c = 0;
+		c=a+b;
+		return c;
+	}
+	public static void main(String args[]) {
+		System.out.println(printHello());
+		System.out.println(add(23,45));
+	}
+}
